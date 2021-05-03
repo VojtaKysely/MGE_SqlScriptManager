1397047e9aef2ef59c590b5cfad89d0b217976f0
Vojta Kyselý <v.kysely@quadient.com>
2021-05-03 18:30:25 +0200

    Commit New Project SQL Script UpdateManager

---
+++ SQLScriptUpdateManager/Manager/Configuration/LoadScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLScriptUpdateManager.Manager
+{
+    public class LoadScript
+    {
+        public void OpenPathToScripts()
+        {
+
+            // Zjistit jak využít FolderBrowserDialog (jak zavolat v prohlížeči FolderDialog).
+
+            //using (var fbd = )
+            //{
+
+            //}
+        }
+    }
+}
---
+++ SQLScriptUpdateManager/Manager/MainManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+
+namespace SQLScriptUpdateManager.Manager
+{
+    public class MainManager
+    {
+        protected void LoadScript(string connectionString, string locationOfPath, string nameOfScript)
+        {
+            FileInfo file = new FileInfo(Path.Combine(locationOfPath, nameOfScript));
+            string script = file.OpenText().ReadToEnd();
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            Server server = new Server(new ServerConnection(conn.ToString()));
+
+            try
+            {
+                server.ConnectionContext.ExecuteNonQuery(script);
+            }
+            catch
+            {
+                throw new Exception("When we try open and load scipt something went wrong");
+            }
+
+        }
+    }
+}
